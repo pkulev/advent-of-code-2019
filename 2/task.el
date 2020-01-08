098c0b41b2d441a1b6067045ceb0fa6ed8c89fbb
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-01-08 13:34:17 +0300

    solve second task
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- 2/task.el
+++ 2/task.el
@@ -45,7 +45,7 @@ ARGNUM says how much elements we must skip before set new PC.")
   (setf (virtual-machine-eof vm) t))
 
 (defun virtual-machine-do-forever (tape)
-  (let ((vm (make-virtual-machine :tape tape)))
+  (let ((vm (make-virtual-machine :tape (copy-seq tape))))
     (while (not (virtual-machine-eof vm))
       (virtual-machine-advance vm))
     vm))
@@ -75,9 +75,22 @@ ARGNUM says how much elements we must skip before set new PC.")
              (virtual-machine-dump-tape vm)
              (elt (virtual-machine-tape vm) 0))))
 
+(defun check-combination (x y tape)
+  "Substitute X and Y values in TAPE and return result of program execution."
+  (setf (elt tape 1) x)
+  (setf (elt tape 2) y)
+  (let ((vm (virtual-machine-do-forever tape)))
+    (elt (virtual-machine-tape vm) 0)))
+
+(defun bruteforce (tape expected-result)
+  (cl-loop for x to 99 append
+           (cl-loop for y to 99
+                    when (= (check-combination x y tape) expected-result)
+                    return (values x y))))
+
 (defun solve-2 (tape expected-result)
-  (let ((vm (make-virtual-machine :tape (copy-seq tape))))
-    (cl-loop for x in)))
+  (multiple-value-bind (x y) (bruteforce tape expected-result)
+    (message "\nsecond: %s" (+ (* 100 x) y))))
 
 (defun solve ()
   (let ((tape (parse-program (get-input))))
@@ -85,7 +98,7 @@ ARGNUM says how much elements we must skip before set new PC.")
     (setf (elt tape 1) 12)
     (setf (elt tape 2) 2)
     (solve-1 tape)
-    (solve-2 tape "19690720")))
+    (solve-2 tape 19690720)))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
